735c7f88ef59d8e8580b360852695475dd332427
Thais <thaismaciielf@hotmail.com>
2025-12-24 01:26:15 -0300

    cleanup: delete de arquivos .py para criação de arquivos .php II

--- api/run.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Instalando dependências...
-pip install -r requirements.txt
-echo Iniciando a API...
-set FLASK_APP=app.py
-set FLASK_ENV=development
-python app.py
-pause
--- api/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Instalando dependências..."
-pip install -r requirements.txt
-echo "Iniciando a API..."
-export FLASK_APP=app.py
-export FLASK_ENV=development
-python app.py
