d2f0db67637400444e87e3f5507d98cbaceee5a6
Thais <thaismaciielf@hotmail.com>
2026-04-27 23:46:01 -0300

    fix(api): add setBasePath no index.php

--- api/index.php
+++ api/index.php
@@ -1,7 +1,5 @@
 <?php
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 /**
@@ -32,6 +30,8 @@ try {
 //}
 
 $app = AppFactory::create();
+
+$app->setBasePath('/api');
 
 
 // ========== MIDDLEWARE DE CORS ==========
